a0ecef98b70c1c25fc8bf95af779005e8a3c0124
Your Name <shelbyfayemo@gmail.com>
2021-08-22 04:47:38 -0400

    created outline with comments to start challenge

---
+++ Data/script.js
@@ -0,0 +1,60 @@
+// var timer = 60; decrease by one for every second that passes
+// var questionIndex = 0; increase everytime changeQuestion() is called
+//var score = 0; increase everytime correct answer selected; 
+// var questions=
+/*[
+    {
+        question: "what is an array", 
+        answers:["a Bucket","not a bucket", "sidewalk chalk", "energy drink"],
+        correctAnswer:"a Bucket"
+    }
+  ]
+  answers[0] //equals "a Bucket"
+*/
+
+
+
+
+
+function startGame(){
+    //runs when user clicks start button
+    //start the timer
+    //set the question and answer choices in html based on first question in the array
+}
+
+function onanswerClick(){
+    //get reference to answer clicked on
+    //check what answer in the array for options matches for this question
+    //compare answer choice to the correct answer to this question
+    //if correct, increase score, increase questionIndex and call setQuestion() 
+    //if wrong, decrease timer and call setQuestion()
+}
+
+function setQuestion(){
+    //set question and answer choices in html
+
+}
+
+function endGame(){
+    //stop timer from decreasing
+    //replace elements on page
+    //show final score
+    //show input to enter initials
+    //show button to submit score
+    //show highscores from local storage
+ /* 
+ {
+     initials:"MM",
+     score:79
+ }
+ //make an array for highscores in local storage if one does not exist
+ //get stringified array from local storage
+ //json.parse on stringified array
+ //push new score object into array
+ //stringify array
+ //set stringified array back into local storage
+ 
+
+
+ */
+}
