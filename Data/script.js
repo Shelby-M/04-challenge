15a5d92f19b55b458cab8f248d4196b9a135442d
Your Name <shelbyfayemo@gmail.com>
2021-08-27 16:52:52 -0400

    created questions array

--- Data/script.js
+++ Data/script.js
@@ -1,26 +1,56 @@
-// var timer = 60; decrease by one for every second that passes
-// var questionIndex = 0; increase everytime changeQuestion() is called
-//var score = 0; increase everytime correct answer selected; 
-// var questions=
-/*[
+var timer = 60; //decrease by one for every second that passes
+var questionIndex = 0; //increase everytime changeQuestion() is called
+var score = 0; //increase everytime correct answer selected; 
+const startButton = document.getElementById('startBtn');
+const nextButton = document.getElementById('nextBtn');
+const questionContainerEl = document.getElementById('question-container')
+const questionEl = document.getElementById('question')
+const answerButtonEl = document.getElementById ('answer-btn')
+let shuffledQuestions, currentQuestionIndex
+
+
+const questionS = [
     {
-        question: "what is an array", 
-        answers:["a Bucket","not a bucket", "sidewalk chalk", "energy drink"],
-        correctAnswer:"a Bucket"
+        question: "what is not a coding language?",
+        answers: ["ABCD", "HTML", "Java Script"],
+        correctAnswer: [0]
+    },
+    {
+        question: "What tag defines an unordered list?",
+        answers: [ "<ul>" , "<li>" , "<hi>"],
+        correctAnswer:[0]
+    },
+    {
+        question: "What does HTML stand for?",
+        answers: ["Huge Typing Man Lion", "Hyper Text Markup Language", "Hyper Typing Markup Language"],
+        correctAnswer:[1]
+    },
+    {
+        question: "What does CSS stand for?",
+        answers: ["Cool Shoes Shaggy", "Create Secure Safety", "Cascading style sheet"],
+        correctAnswer:[2]
+    },
+    { 
+          question: "What does || stand for?",
+        answers: ["Of", "Or", "For"],
+        correctAnswer:[1]
     }
-  ]
-  answers[0] //equals "a Bucket"
-*/
+    
+    ]
 
-
-
-
+startButton.addEventListener( 'click', startGame);
 
 function startGame(){
-    //runs when user clicks start button
+    console.log('started'); //runs when user clicks start button
+    startButton.classList.add('hide');
+    shuffledQuestions = questionS.sort(() => Math.random() - .5) 
+    questionContainerEl.classList.remove('hide')
+    currentQuestionIndex = 0;
+    setNextQuestion()
     //start the timer
     //set the question and answer choices in html based on first question in the array
 }
+
 
 function onanswerClick(){
     //get reference to answer clicked on
@@ -30,8 +60,13 @@ function onanswerClick(){
     //if wrong, decrease timer and call setQuestion()
 }
 
-function setQuestion(){
+function setNextQuestion(){
+    showQuestion(shuffledQuestions[currentQuestionIndex])
     //set question and answer choices in html
+
+}
+
+function showQuestion (question){
 
 }
 
@@ -42,6 +77,12 @@ function endGame(){
     //show input to enter initials
     //show button to submit score
     //show highscores from local storage
+
+
+
+
+
+
  /* 
  {
      initials:"MM",
